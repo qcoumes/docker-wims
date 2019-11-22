c15dc25bad1e3b32b8b8bbf7ec5f9a94fc17e361
pl-test <root@pl-test.u-pem.fr>
2019-11-22 15:49:32 +0100

    Fix command getting number of processor

--- restart_wims.sh
+++ restart_wims.sh
@@ -1,4 +1,4 @@
 docker container rm wims -f
-docker run -itd --cpuset-cpus=$(($(cat /proc/cpuinfo | grep processor | wc -l) - 1)) -p 7777:80 --name wims wims
+docker run -itd --cpuset-cpus=$(($(cat /proc/cpuinfo | grep -e "processor\s*:\s*\d*" | wc -l) - 1)) -p 7777:80 --name wims wims
 docker exec -it wims ./bin/apache-config
 docker exec -it wims service apache2 restart
