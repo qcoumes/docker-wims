e3f1b79c2eb02b72b0ba6ce77b0a300f2f833be0
C. Quentin <qcoumes@etud.u-pem.fr>
2020-07-24 19:01:56 +0200

    Update restart_wims.sh

--- restart_wims.sh
+++ restart_wims.sh
@@ -1,4 +1,4 @@
-docker container rm wims -f
-docker run -itd --cpuset-cpus=$(($(cat /proc/cpuinfo | grep -e "processor\s*:\s*\d*" | wc -l) - 1)) -p 7777:80 --name wims wims
-docker exec -it wims ./bin/apache-config
-docker exec -it wims service apache2 restart
+docker container rm wims-minimal -f
+docker run -itd --cpuset-cpus=$(($(cat /proc/cpuinfo | grep -e "processor\s*:\s*\d*" | wc -l) - 1)) -p 7777:80 --name wims-minimal wims-minimal
+docker exec -it wims-minimal ./bin/apache-config
+docker exec -it wims-minimal service apache2 restart
